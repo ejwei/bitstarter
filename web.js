89d81d5d0ce2535acf3c84f8110e3ba4e97fa21a
unknown2x <ericwei@berkeley.edu>
2013-07-09 07:51:30 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 
-var buf = new Buffer(fs.readFileSync('index.htm;','utf-8'));
+var buf = new Buffer(fs.readFileSync("index.html","utf-8"));
 
 var app = express.createServer(express.logger());
 
